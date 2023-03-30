caddc53a4a1880ab458c730c808e4dd8b2a6706f
Rakib Hossen <rakibhossen1660@gmail.com>
2023-03-30 10:11:51 +0600

    added calculation

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = ({cart}) => {
+    // const cart = props.cart; //option-1
+    // const {cart} = props; //option-2
+    console.log(cart); 
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+
+    const tax = totalPrice*7/100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
+  return (
+    <div className="cart">
+      <h4>Order summary</h4>
+      <p>Selected items: {cart.length}</p>
+      <p>Total price: ${totalPrice}</p>
+      <p>Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h6>Grand Total: ${grandTotal} </h6>
+    </div>
+  );
+};
+
+export default Cart;
